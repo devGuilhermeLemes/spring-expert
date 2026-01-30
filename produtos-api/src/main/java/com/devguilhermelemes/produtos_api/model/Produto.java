a6fceb0155f9e9f6f97f36b0d25777f76d92e7e7
devGuilhermeLemes <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>
2026-01-30 15:17:13 -0300

    Configuração do application.yml e db h2

--- produtos-api/src/main/java/com/devguilhermelemes/produtos_api/model/Produto.java
+++ produtos-api/src/main/java/com/devguilhermelemes/produtos_api/model/Produto.java
@@ -1,18 +1,25 @@
 package com.devguilhermelemes.produtos_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 // POJO - Plain Old Java Object
+@Entity
+@Table(name = "produto")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Produto {
 
+    @Id
     private String id;
+    
     private String nome;
     private String descricao;
     private Double preco;
