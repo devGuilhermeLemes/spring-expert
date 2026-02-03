26216e7d8155a6f90877c8d72ba3d9b76bfbc914
devGuilhermeLemes <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>
2026-02-03 19:39:38 -0300

    Busca por ID no db

--- produtos-api/src/main/java/com/devguilhermelemes/produtos_api/controller/ProdutoController.java
+++ produtos-api/src/main/java/com/devguilhermelemes/produtos_api/controller/ProdutoController.java
@@ -1,5 +1,10 @@
 package com.devguilhermelemes.produtos_api.controller;
 
+// import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +17,7 @@ import com.devguilhermelemes.produtos_api.repository.ProdutoRepository;
 @RequestMapping("produtos")
 public class ProdutoController {
 
-
+    // * Injeção de depêndencia
     private final ProdutoRepository produtoRepository;
 
     public ProdutoController(ProdutoRepository produtoRepository) {
@@ -23,7 +28,9 @@ public class ProdutoController {
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto Recebido: " + produto);
 
-        // ! Controller não deve ter regra de geração de identidade | Se amanhã você salvar produto em outro fluxo → quebra | Viola SRP (Single Responsibility Principle)
+        // ! Controller não deve ter regra de geração de identidade | Se amanhã você
+        // salvar produto em outro fluxo → quebra | Viola SRP (Single Responsibility
+        // Principle)
         // var id = UUID.randomUUID().toString(); MÁ PRATICA
         // produto.setId(id); MÁ PRATICA
         // * Deixar o Hibernate gerar UUID automaticamente
@@ -32,4 +39,11 @@ public class ProdutoController {
         return produto;
     }
 
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") UUID id) {
+        // Optional<Produto> produto = produtoRepository.findById(id);
+        // return produto.isPresent() ? produto.get() : null;
+
+        return produtoRepository.findById(id).orElse(null);
+    }
 }
--- produtos-api/src/main/java/com/devguilhermelemes/produtos_api/repository/ProdutoRepository.java
+++ produtos-api/src/main/java/com/devguilhermelemes/produtos_api/repository/ProdutoRepository.java
@@ -1,9 +1,11 @@
 package com.devguilhermelemes.produtos_api.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devguilhermelemes.produtos_api.model.Produto;
 
-public interface ProdutoRepository extends JpaRepository <Produto, String> {
+public interface ProdutoRepository extends JpaRepository <Produto, UUID> {
 
 }
